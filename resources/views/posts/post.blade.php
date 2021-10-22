10191428be477a78b919f80228384e1ef15c2dc8
michafvdw <0990603@hr.nl>
2021-10-22 13:46:13 +0200

    Delete function
    Delete function works. CRUD is now complete.

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -9,6 +9,15 @@
             >
                 Edit
             </a>
+
+        <a
+            href="{{ route('posts.destroy',$post, [$post->slug])}}"
+            class="button is-info is-outlined"
+        >
+            Delete
+        </a>
+
+
             <h1> {{ $post->title }} </h1>
             <p> {{ $post->body }} </p>
 
